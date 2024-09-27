8c8eda29ba10d256a599b4e0b095829fe1298a16
PedroHTP <Pedrohtpgbi2007@gmail.com>
2024-09-27 18:05:59 -0300

    O código supremo e definito.
    Essa é uma das coisas mais lindas que já vi.

--- exercicios/ex006/index.php
+++ exercicios/ex006/index.php
@@ -15,18 +15,27 @@
         <h1>A soma de dois valores</h1>
         <form action="<?=$_SERVER['PHP_SELF']?>" method="post">
             <label for="v1">Valor 1</label>
-            <input type="number" name="v1" id="idv1" value="<?=$valor1?>">
+            <input type="number" name="v1" id="idv1" value="<?=$valor1?>" step="any">
             <label for="v2">Valor 2</label>
-            <input type="number" name="v2" id="idv2"  value="<?=$valor2?>">
+            <input type="number" name="v2" id="idv2"  value="<?=$valor2?>"  step="any">
             <input type="submit" value="Calcular">
         </form>
     </main>
         <?php 
             if ($valor1!= "N" or $valor2 != "N") {
-                $soma = $valor1 + $valor2;
+            $soma = $valor1 + $valor2;
+                $decimal1 = substr(strpbrk($valor1, '.'), 1);
+                $cont1 = iconv_strlen($decimal1);
+                $decimal2 = substr(strpbrk($valor2, '.'), 1);
+                $cont2 = iconv_strlen($decimal2);
+                    if ($cont1 > $cont2) {
+                        $contr = $cont1;
+                    } else {
+                        $contr = $cont2;
+                    }
                 echo "<section>
                     <h2>Resultado da soma</h2>
-                    <p>O soma de $valor1 + $valor2 é igual a $soma.</p>
+                    <p>O soma de " . number_format($valor1, $cont1, ",", ".") . " + " . number_format($valor2, $cont2, ",", ".") . " é igual a " . number_format($soma, $contr, ",", ".") . "</p>
                 </section>";
             } 
         ?>
