195b4e92f08cf9650d5d2126462dca805f5dae32
PedroHTP <Pedrohtpgbi2007@gmail.com>
2024-09-28 18:29:31 -0300

    Desafio 10

---
+++ desafios/d010/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Idade</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <?php 
+        $atual = date('Y');
+        $nasce = $_GET ['nasce'] ?? false;
+        $ano = $_GET ['ano'] ?? $atual;
+    ?>
+    <main>
+        <h1>Calcurando a sua idade</h1>
+        <form action="<?=$_SERVER['PHP_SELF']?>" method="get">
+            <label for="nasce">Em que ano você nasceu?</label>
+            <input type="number" name="nasce" id="idnasce" value="<?=$nasce?>">
+            <label for="ano">Quer saber sua idade em que ano? (atualmente estamos em <strong><?=$atual?></strong>)</label>
+            <input type="number" name="ano" id="idano" value="<?=$ano?>">
+            <input type="submit" value="Calcular minha idade">
+        </form>
+    </main>
+    <?php 
+        if ($nasce != false) {
+            $idade = $ano - $nasce;
+            echo "
+                <section>
+                    <h1>Resultado</h1>
+                    <p>Quem nasceu em ". $nasce ." vai ter <strong>" . $idade . " anos</strong> em " . $ano . "</p>
+                </section>
+            ";
+        }
+    ?>
+</body>
+
+</html>
