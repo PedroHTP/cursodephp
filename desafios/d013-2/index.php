1853f3411647609dbaf7f85801459f468d11fcb1
PedroHTP <Pedrohtpgbi2007@gmail.com>
2024-09-29 19:26:00 -0300

    Atualização do desafio 13

--- desafios/d013-2/index.php
+++ desafios/d013-2/index.php
@@ -23,8 +23,7 @@
         <h1>Caixa Eletrônico</h1>
         <form action="<?=$_SERVER['PHP_SELF']?>" method="get">
             <label for="sacar">Qual valor você deseja sacar? (R$)*</label>
-            <input type="number" name="sacar" id="idsacar" step="5" value="<?=$valor?>" min="5">
-            <p>*Notas disponíveis: R$100, R$50, R$10, R$5</p>
+            <input type="number" name="sacar" id="idsacar" value="<?=$valor?>" min="1">
             <input type="submit" value="Sacar">
         </form>
         <img src="" alt="">
@@ -35,9 +34,15 @@
             $resto = $valor % 100;
             $cinq = intdiv($resto, 50);
             $resto = $resto % 50;
+            $vinte = intdiv($resto, 20);
+            $resto = $resto % 20;
             $dez = intdiv($resto, 10);
             $resto = $resto % 10;
             $cinco = intdiv($resto, 5);
+            $resto = $resto % 5;
+            $dois = intdiv($resto, 2);
+            $resto = $resto % 2;
+            $um = $resto;
             
             print "<section>
                 <h2>Saque de ". numfmt_format_currency($padrao, $valor, "BRL") ." realizado</h2>
@@ -52,6 +57,11 @@
                 print "<img src=\"imagens\\50-reais.jpg\" alt=\"\"></img>" . " x" . $cinq;
                 print "</li>";
             }
+            if ($vinte>0) {
+                print "<li>";
+                print "<img src=\"imagens\\20-reais.jpg\" alt=\"\"></img>" . " x" . $vinte;
+                print "</li>";
+            }
             if ($dez>0) {
                 print "<li>";
                 print "<img src=\"imagens\\10-reais.jpg\" alt=\"\"></img>" . " x" . $dez;
@@ -60,6 +70,16 @@
             if ($cinco>0) {
                 print "<li>";
                 print "<img src=\"imagens\\5-reais.jpg\" alt=\"\"></img>" . " x" . $cinco;
+                print "</li>";
+            }
+            if ($dois>0) {
+                print "<li>";
+                print "<img src=\"imagens\\2-reais.jpg\" alt=\"\"></img>" . " x" . $dois;
+                print "</li>";
+            }
+            if ($um>0) {
+                print "<li>";
+                print "<img src=\"imagens\\1-real.jpg\" alt=\"\"></img>" . " x" . $um;
                 print "</li>";
             }
             print "</ul></section>";
