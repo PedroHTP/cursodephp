2cd0324c358601e92ef9cb896d60706ab6ca698d
Pedro Henrique Teixeira Pião <Pedrohtpgbi2007@gmail.com>
2024-12-26 01:51:53 -0300

    Update atv07.php

--- Node/atividades/atv07.php
+++ Node/atividades/atv07.php
@@ -6,6 +6,21 @@
     <title>SuperGlobais</title>
 </head>
 <body>
-    
+    <?php 
+        $variavelg1 = 10;
+        $variavelg2 = 25;
+            function soma() {
+                echo $GLOBALS ['variavelg1'] + $GLOBALS ['variavelg2'];
+            }
+        
+        // Superglobal: Server
+
+        echo $_SERVER['PHP_SELF']."<br>";
+        echo $_SERVER['SERVER_NAME']."<br>";
+        echo $_SERVER['SCRIPT_FILENAME']."<br>";
+        echo $_SERVER['DOCUMENT_ROOT']."<br>";
+        echo $_SERVER['SERVER_PORT']."<br>";
+        echo $_SERVER['REMOTE_ADDR']."<br>";
+    ?>
 </body>
 </html>
