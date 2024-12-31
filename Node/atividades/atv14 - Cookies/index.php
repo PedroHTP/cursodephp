23466259f55f22b8b36be96d3ad70df5f59b4082
Pedro Henrique Teixeira Pião <Pedrohtpgbi2007@gmail.com>
2024-12-31 16:52:17 -0300

    Rename file

---
+++ Node/atividades/atv14 - Cookies/index.php
@@ -0,0 +1,10 @@
+<?php 
+// COOKIE
+    function horas($horas) {
+        $segundos = $horas * 3600;
+        return $segundos;
+    }
+// setcookie ($nome-do-cookie, $valor-do-cookie, time()+3600);
+
+setcookie('user', 'Pedro Henrique Teixeira Pião', time()+horas(2));
+setcookie('email', 'pedrohtpgbi2007@gmail.com', time()+horas(2));
